bc3cfc1a1db7d5ca2ec445a1053bb47d3ced4ff6
Michael Auslander <michael.auslander@gmail.com>
2015-10-28 12:54:49 -0400

    Created functions as empty shells.

--- droneControlAPI.py
+++ droneControlAPI.py
@@ -1,2 +1,115 @@
-# This API is used to control the drone through Python and DroneAPI in MAVProxy.
+# This API is used to control the drone through Python and DroneAPI in MAVProxy. Please see the Google Doc for documentation on what these functions do.
 
+
+def takeoff():
+
+    return;
+
+
+def land():
+
+    return;
+
+def altDecV(int):
+
+    return;
+
+def altDecP():
+
+    return;
+
+def altIncV(int):
+
+    return;
+
+def altIncP():
+
+    return;
+
+def leftV(int):
+    
+    return;
+   
+def leftP():
+
+    return;
+
+def rightV(int):
+
+    return;
+
+def rightP():
+
+    return;
+
+def forwardV(int):
+
+    return;
+
+def forwardP():
+
+    return;
+
+def reverseV(int):
+
+    return;
+
+def reverseP():
+
+    return;
+
+def spinLeftV(int):
+
+    return;
+
+def spinLeftP():
+
+    return;
+
+def spinRightV(int):
+
+    return;
+
+def spinRightP():
+
+    return;
+
+def releaseControlAlt():
+
+    return;
+
+def releaseControlYaw():
+
+    return;
+
+def releaseControlSide():
+
+    return;
+
+def releaseControlFB():
+
+    return;
+
+def releaseControlAll():
+
+    return;
+
+def getAltitude():
+
+    return;
+
+def getHeading():
+
+    return;
+
+def getAngle():
+
+    return;
+
+def getGPSPosition():
+
+    return;
+
+
+# GPS COORDINATES
+# Input GPS coordinates, set mode to auto
