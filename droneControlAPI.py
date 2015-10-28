a5e0249b84fef31e0e2ceefabd3f8fcba736a485
Michael Auslander <michael.auslander@gmail.com>
2015-10-28 17:45:09 -0400

    Added definitions for Forward/Back, Left/Right, Spin. Added definitions for Release Control.

--- droneControlAPI.py
+++ droneControlAPI.py
@@ -1,4 +1,4 @@
-# This API is used to control the drone through Python and DroneAPI in MAVProxy. Please see the Google Doc for documentation on what these functions do.
+﻿# This API is used to control the drone through Python and DroneAPI in MAVProxy. Please see the Google Doc for documentation on what these functions do.
 
 
 def takeoff():
@@ -36,72 +36,84 @@ def altIncP():
     vehicle.channel_override = {"3":1500}
     return;
 
-def leftV(int):
-    
+def leftV( pcnt ):
+    vehicle.channel_override = {"1":(1500-(pcnt*5))}
     return;
    
 def leftP():
-
+    vehicle.channel_override = {"1":1250}
+    sleep(0.5)
+    vehicle.channel_override = {"1":1500}
     return;
 
-def rightV(int):
-
+def rightV( pcnt ):
+    vehicle.channel_override = {"1":(1500+(pcnt*5))}
     return;
 
 def rightP():
-
+    vehicle.channel_override = {"1":1750}
+    sleep(0.5)
+    vehicle.channel_override = {"1":1500}
     return;
 
-def forwardV(int):
-
+def forwardV( pcnt ):
+    vehicle.channel_override = {"2":(1500+(pcnt*5))}
     return;
 
 def forwardP():
-
+    vehicle.channel_override = {"2":1750}
+    sleep(0.5)
+    vehicle.channel_override = {"2":1500}
     return;
 
-def reverseV(int):
-
+def reverseV( pcnt ):
+    vehicle.channel_override = {"2":(1500-(pcnt*5))}
     return;
 
 def reverseP():
-
+    vehicle.channel_override = {"2":1250}
+    sleep(0.5)
+    vehicle.channel_override = {"2":1500}
     return;
 
-def spinLeftV(int):
-
+def spinLeftV( pcnt ):
+    vehicle.channel_override = {"4":(1500-(pcnt*5))}
     return;
 
 def spinLeftP():
-
+    vehicle.channel_override = {"4":1250}
+    sleep(0.5)
+    vehicle.channel_override = {"4":1500}
     return;
 
-def spinRightV(int):
-
+def spinRightV( pcnt ):
+    vehicle.channel_override = {"4":(1500+(pcnt*5))}
     return;
 
 def spinRightP():
-
+    vehicle.channel_override = {"4":1750}
+    sleep(0.5)
+    vehicle.channel_override = {"4":1500}
     return;
 
 def releaseControlAlt():
-
+    vehicle.channel_override = {"3":0}
     return;
 
 def releaseControlYaw():
-
+    vehicle.channel_override = {"4":0}
     return;
 
 def releaseControlSide():
-
+    vehicle.channel_override = {"1":0}
     return;
 
 def releaseControlFB():
-
+    vehicle.channel_override = {"2":0}
     return;
 
 def releaseControlAll():
-
+    vehicle.channel_override = {"1":0, "2":0, "3":0, "4":0}
     return;
 
 def getAltitude():
