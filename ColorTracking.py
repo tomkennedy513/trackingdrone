a66d28417bc177d01ba09ff9a6aa58841c631215
Tom Kennedy <tkenned8@villanova.edu>
2015-12-03 15:17:08 -0500

    color

--- ColorTracking.py
+++ ColorTracking.py
@@ -9,33 +9,38 @@ iLowS = 0;
 iHighS = 255;
 iLowV = 0;
 iHighV = 255;
+Track = False;
 
 
 def main():
-	global frame, roiPts, inputMode
+	global frame, roiPts, Track
 	
 	camera = cv2.VideoCapture(0)
 	width, height = 600 , 480
 	camera.set(3, width)
 	camera.set(4, height)
-	
-
 	cv2.namedWindow("frame")
+	cv2.namedWindow("preview")
 	
   
- 	cv2.cv.CreateTrackbar("LowH", "frame", iLowH, 179, main);
- 	cv2.cv.CreateTrackbar("HighH", "frame", iHighH, 179, main);
+ 	cv2.cv.CreateTrackbar("LowH", "preview", iLowH, 179, null)
+ 	cv2.cv.CreateTrackbar("HighH", "preview", iHighH, 179, null)
 
-  	cv2.cv.CreateTrackbar("LowS", "frame", iLowS, 255, main); 
- 	cv2.cv.CreateTrackbar("HighS", "frame", iHighS, 255, main);
+  	cv2.cv.CreateTrackbar("LowS", "preview", iLowS, 255, null)
+ 	cv2.cv.CreateTrackbar("HighS", "preview", iHighS, 255, null)
 
-  	cv2.cv.CreateTrackbar("LowV", "frame", iLowV, 255, main);
- 	cv2.cv.CreateTrackbar("HighV", "frame", iHighV, 255, main);
+  	cv2.cv.CreateTrackbar("LowV", "preview", iLowV, 255, null)
+ 	cv2.cv.CreateTrackbar("HighV", "preview", iHighV, 255, null)
 	
 	termination = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 0)
 	roiBox = None
-	while True:
+	'''while True:
 		(grabbed, frame) = camera.read()
+		roiHist2 = cv2.calcHist([frame], [0], None, [16], [0, 180])
+		roiHist2 = cv2.normalize(roiHist2, roiHist2, 0, 255, cv2.NORM_MINMAX)
+		hsv2 = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+		backProj2 = cv2.calcBackProject([hsv2], [0], roiHist2, [0, 180], 1)
+		cv2.imshow("preview", backProj2)
 		if not grabbed:
 			break
 		if roiBox is not None:
@@ -60,27 +65,26 @@ def main():
 				pts[1][0] * pts[0][1]) - (pts[2][0] * pts[1][1]) - (pts[3][0] * pts[2][1]) - (pts[0][0] * pts[3][1]))
 				print cPt
 				cv2.polylines(frame, [pts], True, (255, 204, 0), 2)
-				#cv2.circle(frame, cPt, 1, (255, 0, 0), 2)
-				#cv2.putText(frame, 'target', (cPtx, (cPty - 7)), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
-				#cv2.putText(frame, str(cPt), (7, 25), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
-				#cv2.putText(frame, str(boxArea), (7, 55), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+				
 
 				#print camera.get(3)
 				#print camera.get(4)
 
 				#print xPid.GenOut(cPtx-camera.get(3)/2)
-				#print yPid.GenOut(cPty-camera.get(4)/2)
+				#print yPid.GenOut(cPty-camera.get(4)/2)'''
 		cv2.imshow("frame", frame)
 		key = cv2.waitKey(1) & 0xFF
 
-		if key == 32 and len(roiPts) < 4:
-			inputMode = True
-			orig = frame.copy()
+		if key == 32 and Track == False:
+			Track = True
+		if key == 32 and Track == True:
+			Track=False
+		
+		
+		if key == ord("q"):
+			break
 
-			while len(roiPts) < 4:
-				cv2.imshow("frame", frame)
-				cv2.waitKey(0)
-
+		if Track == True:
 			roiPts = np.array(roiPts)
 			s = roiPts.sum(axis=1)
 			tl = roiPts[np.argmin(s)]
@@ -95,15 +99,13 @@ def main():
 
 			roiBox = (tl[0], tl[1], br[0], br[1])
 			print roiBox
-		
-		
-		elif key == ord("q"):
-			break
 
 
 	camera.release()
 	cv2.destroyAllWindows()
 
+def null(null):
+	print null
 
 if __name__ == "__main__":
 	main()
