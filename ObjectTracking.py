4e16e7c07693c2e4e3e11c7bebd84ea3daf4605d
Shan Shi <a455898334@gmail.com>
2015-12-03 15:43:25 -0500

    master conflict resolution

--- ObjectTracking.py
+++ ObjectTracking.py
@@ -1,19 +1,5 @@
 ﻿import numpy as np
 import cv2
-<<<<<<< HEAD
-import Tkinter as tk
-import multiprocessing
-
-#import pid
-#import droneControlAPI
-
-frame = None
-roiPts = []
-inputMode = False
-xPts = []
-yPts = []
-
-=======
 import sys
 import os
 import time
@@ -23,8 +9,6 @@ from dronekit import connect
 sys.path.append("/home/shan/code/trackingdrone/") #change this for the pi
 from droneControlAPI import mikeObject
 import pid
->>>>>>> refs/remotes/origin/master
-
 
 
 def selectROI(event, x, y, flags, param):
@@ -44,108 +28,11 @@ api=local_connect()
 vehicle = api.get_vehicles()[0]
 drone = mikeObject()
 
-<<<<<<< HEAD
-def main():
-	global frame, roiPts, inputMode
-	root = tk.Tk()
-	root.mainloop()
-	camera = cv2.VideoCapture(0)
-	width, height = 600 , 480
-	camera.set(3, width)
-	camera.set(4, height)
-	
-	#xPid = pid.PID(.1,.1,.1)
-	#yPid = pid.PID(.1,.1,.1)
-
-	cv2.namedWindow("frame")
-	#cv2.namedWindow("HSV")
-	#cv2.namedWindow("Back Projection")
-
-	cv2.setMouseCallback("frame", selectROI)
-	termination = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 0)
-	roiBox = None
-	while True:
-		(grabbed, frame) = camera.read()
-		if not grabbed:
-			break
-		if roiBox is not None:
-			hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-			#cv2.imshow("HSV", hsv)
-			backProj = cv2.calcBackProject([hsv], [0], roiHist, [0, 180], 1)
-			#cv2.imshow("Back Projection", backProj)
-
-			(r, roiBox) = cv2.meanShift(backProj, roiBox, termination)
-			if not r:
-				print "error"
-				cv2.putText(frame, "Object Out of Frame!", (7, 25), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
-			else:
-				(r, roiBox) = cv2.CamShift(backProj, roiBox, termination)
-				pts = np.int0(cv2.cv.BoxPoints(r))
-			# print pts
-				cPtx = (pts[0][0] + pts[1][0] + pts[2][0] + pts[3][0]) / 4
-				cPty = (pts[0][1] + pts[1][1] + pts[2][1] + pts[3][1]) / 4
-				cPt = (cPtx, cPty)
-				boxArea = .5 * (
-				(pts[0][0] * pts[1][1]) + (pts[1][0] * pts[2][1]) + (pts[2][0] * pts[3][1]) + (pts[3][0] * pts[0][1]) - (
-				pts[1][0] * pts[0][1]) - (pts[2][0] * pts[1][1]) - (pts[3][0] * pts[2][1]) - (pts[0][0] * pts[3][1]))
-				print cPt
-				cv2.polylines(frame, [pts], True, (255, 204, 0), 2)
-				#cv2.circle(frame, cPt, 1, (255, 0, 0), 2)
-				#cv2.putText(frame, 'target', (cPtx, (cPty - 7)), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
-				#cv2.putText(frame, str(cPt), (7, 25), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
-				#cv2.putText(frame, str(boxArea), (7, 55), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
-
-				#print camera.get(3)
-				#print camera.get(4)
-
-				#print xPid.GenOut(cPtx-camera.get(3)/2)
-				#print yPid.GenOut(cPty-camera.get(4)/2)
-		cv2.imshow("frame", frame)
-		key = cv2.waitKey(1) & 0xFF
-
-		if key == 32 and len(roiPts) < 4:
-			inputMode = True
-			orig = frame.copy()
-
-			while len(roiPts) < 4:
-				cv2.imshow("frame", frame)
-				cv2.waitKey(0)
-
-			roiPts = np.array(roiPts)
-			s = roiPts.sum(axis=1)
-			tl = roiPts[np.argmin(s)]
-			br = roiPts[np.argmax(s)]
-			print roiPts
-
-			roi = orig[tl[1]:br[1], tl[0]:br[0]]
-			roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-			# roi = cv2.cvtColor(roi, cv2.COLOR_BGR2LAB)
-
-			roiHist = cv2.calcHist([roi], [0], None, [16], [0, 180])
-			roiHist = cv2.normalize(roiHist, roiHist, 0, 255, cv2.NORM_MINMAX)
-
-			roiBox = (tl[0], tl[1], br[0], br[1])
-			print roiBox
-		# choose new target without resetting
-		elif key == ord("r"):
-			frame = None
-			roiPts = []
-			inputMode = False
-			xPts = []
-			yPts = []
-		elif key == ord("q"):
-			break
-
-
-	camera.release()
-	cv2.destroyAllWindows()
-=======
 frame = None
 roiPts = []
 inputMode = False
 xPts = []
 yPts = []
->>>>>>> refs/remotes/origin/master
 
 camera = cv2.VideoCapture(0)
 
